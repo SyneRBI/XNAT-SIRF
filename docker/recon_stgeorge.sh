fcbaa9aea9f5538f79c8684865fceea8987119af
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2026-04-29 13:51:30 +0000

    added script from project 5

---
+++ docker/recon_stgeorge.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+. /opt/SIRF-SuperBuild/INSTALL/bin/env_sirf.sh
+gadgetron &
+# python /workdir/reco_scripts/mr_direct_recon.py /input /output
+python /workdir/reco_scripts/stgeorges_pipeline.py
